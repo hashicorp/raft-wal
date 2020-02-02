b1df87a0cd9475f2883d1d6730405909cce225ec
Mahmood Ali <mahmood@hashicorp.com>
2020-02-01 20:09:47 -0500

    some more tests

--- log/segment.go
+++ log/segment.go
@@ -2,9 +2,14 @@ package log
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
+	"compress/zlib"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"hash/crc32"
+	"io"
 	"os"
 	"path/filepath"
 	"sync"
@@ -13,7 +18,18 @@ import (
 )
 
 var (
-	crc32Table = crc32.MakeTable(crc32.Castagnoli)
+	crc32Table          = crc32.MakeTable(crc32.Castagnoli)
+	segment_magic_bytes = []byte("__RAFT_WAL__")
+)
+
+const (
+	segment_header_size   = 62
+	segmentVersion        = 1
+	segmentDataInitOffset = 512
+)
+
+var (
+	errOutOfSequence = errors.New("out of sequence index")
 )
 
 type segment struct {
@@ -21,6 +37,7 @@ type segment struct {
 
 	baseIndex    uint64
 	openForWrite bool
+	config       LogConfig
 
 	offsets    []uint32
 	nextOffset uint32
@@ -33,7 +50,14 @@ func segmentName(baseIndex uint64) string {
 	return fmt.Sprintf("wal-%016x.log", baseIndex)
 }
 
-func newSegment(dir string, baseIndex uint64, forWrite bool, config *LogConfig) (*segment, error) {
+func setSegmentHeader(b [segment_header_size]byte, baseIndex uint64, config LogConfig) {
+	copy(b[:], segment_magic_bytes)
+	b[16] = segmentVersion
+	b[17] = byte(config.Compression)
+	binary.BigEndian.PutUint64(b[18:26], baseIndex)
+}
+
+func newSegment(dir string, baseIndex uint64, forWrite bool, config LogConfig) (*segment, error) {
 	fp := filepath.Join(dir, segmentName(baseIndex))
 
 	rdf := os.O_RDONLY
@@ -46,19 +70,27 @@ func newSegment(dir string, baseIndex uint64, forWrite bool, config *LogConfig) 
 		return nil, err
 	}
 
-	fileutil.Preallocate(f, 64*1024*1024, true)
+	if err := fileutil.Preallocate(f, 64*1024*1024, true); err != nil {
+		return nil, err
+	}
 
-	var header [512]byte
+	var header [segment_header_size]byte
+	setSegmentHeader(header, baseIndex, config)
 	if _, err := f.Write(header[:]); err != nil {
 		os.Remove(fp)
+		return nil, err
+	}
+
+	if _, err := f.Seek(segmentDataInitOffset, 0); err != nil {
 		return nil, err
 	}
 
 	return &segment{
 		baseIndex:    baseIndex,
 		openForWrite: forWrite,
+		config:       config,
 		offsets:      make([]uint32, 0, 512),
-		nextOffset:   512,
+		nextOffset:   segmentDataInitOffset,
 		f:            f,
 		bw:           bufio.NewWriterSize(f, 4096),
 	}, nil
@@ -83,33 +115,92 @@ func (s *segment) GetLog(index uint64, out []byte) (int, error) {
 		rl = s.offsets[li+1] - s.offsets[li]
 	}
 
-	bytes := make([]byte, rl)
-	_, err := s.f.ReadAt(bytes, int64(s.offsets[li]))
+	record := make([]byte, rl)
+	_, err := s.f.ReadAt(record, int64(s.offsets[li]))
 	if err != nil {
 		return 0, err
 	}
-	lIndex := binary.BigEndian.Uint64(bytes[:8])
+	lIndex := binary.BigEndian.Uint64(record[:8])
 	if lIndex != index {
 		return 0, fmt.Errorf("index mismatch %v != %v", lIndex, index)
 	}
 
-	lChecksum := binary.BigEndian.Uint32(bytes[8:12])
-	lLength, lPadding := decodeLength(binary.BigEndian.Uint32(bytes[12:16]))
+	lChecksum := binary.BigEndian.Uint32(record[8:12])
+	lLength, lPadding := decodeLength(binary.BigEndian.Uint32(record[12:16]))
 
 	if 16+lLength+lPadding != rl {
 		return 0, fmt.Errorf("mismatch length: %v != %v", 16+lLength+lPadding, rl)
 	}
 
-	fChecksum := crc32.Checksum(bytes[16:16+lLength], crc32Table)
+	fChecksum := crc32.Checksum(record[16:16+lLength], crc32Table)
 	if lChecksum != fChecksum {
 		return 0, fmt.Errorf("checksums mismatch")
 	}
 
-	n := copy(out, bytes[16:16+lLength])
-	if uint32(n) != lLength {
-		return n, fmt.Errorf("bytes too small: %v != %v; %v", n, lLength, bytes)
+	data, err := uncompress(s.config.Compression, record[16:16+lLength])
+	if err != nil {
+		return 0, err
+	}
+
+	n := copy(out, data)
+	if n != len(data) {
+		return n, fmt.Errorf("record too small: %v != %v; %v", n, lLength, record)
 	}
 	return n, nil
+}
+
+func uncompress(compressionType LogCompression, data []byte) ([]byte, error) {
+	if compressionType == LogCompressionNone {
+		return data, nil
+	}
+
+	var buf bytes.Buffer
+	var reader io.ReadCloser
+	var err error
+
+	switch compressionType {
+	case LogCompressionZlib:
+		reader, err = zlib.NewReader(bytes.NewReader(data))
+	case LogCompressionGZip:
+		reader, err = gzip.NewReader(bytes.NewReader(data))
+	default:
+		return nil, fmt.Errorf("unknown compression typoe: %v", compressionType)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = io.Copy(&buf, reader); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func compress(compressionType LogCompression, data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	var writer io.WriteCloser
+	switch compressionType {
+	case LogCompressionNone:
+		return data, nil
+	case LogCompressionZlib:
+		writer = zlib.NewWriter(&buf)
+	case LogCompressionGZip:
+		writer = gzip.NewWriter(&buf)
+	default:
+		return nil, fmt.Errorf("unknown compression typoe: %v", compressionType)
+	}
+
+	if _, err := writer.Write(data); err != nil {
+		return nil, err
+	}
+
+	if err := writer.Close(); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
 
 func (s *segment) StoreLogs(index uint64, next func() []byte) error {
@@ -127,7 +218,7 @@ func (s *segment) StoreLogs(index uint64, next func() []byte) error {
 	var writtenEntries int
 
 	if int(index-s.baseIndex) != len(s.offsets) {
-		return fmt.Errorf("out of sequence write: %v != %v", index-s.baseIndex, len(s.offsets))
+		return errOutOfSequence
 	}
 
 	s.bw.Reset(s.f)
@@ -136,6 +227,15 @@ func (s *segment) StoreLogs(index uint64, next func() []byte) error {
 	offsetLengths := len(s.offsets)
 
 	for data := next(); data != nil; data = next() {
+
+		if s.config.Compression != LogCompressionNone {
+			source := data
+			data, err = compress(s.config.Compression, source)
+			if err != nil {
+				return err
+			}
+		}
+
 		// prepare header
 		checksum := crc32.Checksum(data, crc32Table)
 
--- log/segment_test.go
+++ log/segment_test.go
@@ -9,39 +9,133 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func stringsIterator(d []string) func() []byte {
+	i := 0
+	return func() []byte {
+		if i >= len(d) {
+			return nil
+		}
+
+		l := []byte(d[i])
+		i++
+		return l
+
+	}
+}
+
 func TestSegment_Basic(t *testing.T) {
 	dir, err := ioutil.TempDir("", "testsegment")
 	require.NoError(t, err)
 	defer os.RemoveAll(dir)
 
-	s, err := newSegment(dir, 1, true, nil)
-	require.NoError(t, err)
-
-	logs := [][]byte{
-		[]byte("log 1"),
-		[]byte("log 2"),
-		[]byte("log 3"),
+	cases := []struct {
+		name   string
+		config LogConfig
+	}{
+		{"basic", LogConfig{}},
+		{"compressed_zlib", LogConfig{Compression: LogCompressionZlib}},
+		{"compressed_gzip", LogConfig{Compression: LogCompressionGZip}},
 	}
 
-	i := 0
-	err = s.StoreLogs(1, func() []byte {
-		if i >= len(logs) {
-			return nil
-		}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s, err := newSegment(dir, 1, true, c.config)
+			require.NoError(t, err)
 
-		l := logs[i]
-		i++
-		return l
-	})
+			logs := []string{
+				"log 1",
+				"log 2",
+				"log 3",
+			}
+
+			err = s.StoreLogs(1, stringsIterator(logs))
+			require.NoError(t, err)
+
+			moreLogs := []string{
+				"log 4",
+				"log 5",
+				"log 6",
+			}
+			err = s.StoreLogs(4, stringsIterator(moreLogs))
+			require.NoError(t, err)
+
+			logs = append(logs, moreLogs...)
+			out := make([]byte, 32)
+			for i, l := range logs {
+				n, err := s.GetLog(uint64(i+1), out)
+				require.NoError(t, err)
+				require.Equal(t, []byte(l), out[:n])
+			}
+
+			err = s.StoreLogs(1, stringsIterator(moreLogs))
+			require.Error(t, err)
+			require.Equal(t, errOutOfSequence, err)
+
+			err = s.StoreLogs(1000, stringsIterator(moreLogs))
+			require.Error(t, err)
+			require.Equal(t, errOutOfSequence, err)
+		})
+	}
+}
+
+func TestSegment_OtherBase(t *testing.T) {
+	dir, err := ioutil.TempDir("", "testsegment")
 	require.NoError(t, err)
+	defer os.RemoveAll(dir)
 
-	out := make([]byte, 32)
-	for i, l := range logs {
-		n, err := s.GetLog(uint64(i+1), out)
-		require.NoError(t, err)
-		require.Equal(t, l, out[:n])
+	cases := []struct {
+		name   string
+		config LogConfig
+	}{
+		{"basic", LogConfig{}},
+		{"compressed_zlib", LogConfig{Compression: LogCompressionZlib}},
+		{"compressed_gzip", LogConfig{Compression: LogCompressionGZip}},
 	}
 
+	baseIndex := uint64(51200)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s, err := newSegment(dir, baseIndex, true, c.config)
+			require.NoError(t, err)
+
+			logs := []string{
+				"log 1",
+				"log 2",
+				"log 3",
+			}
+
+			err = s.StoreLogs(baseIndex, stringsIterator(logs))
+			require.NoError(t, err)
+
+			moreLogs := []string{
+				"log 4",
+				"log 5",
+				"log 6",
+			}
+			err = s.StoreLogs(baseIndex+3, stringsIterator(moreLogs))
+			require.NoError(t, err)
+
+			logs = append(logs, moreLogs...)
+			out := make([]byte, 32)
+			for i, l := range logs {
+				n, err := s.GetLog(uint64(i)+baseIndex, out)
+				require.NoError(t, err)
+				require.Equal(t, []byte(l), out[:n])
+			}
+
+			err = s.StoreLogs(1, stringsIterator(moreLogs))
+			require.Error(t, err)
+			require.Equal(t, errOutOfSequence, err)
+
+			err = s.StoreLogs(baseIndex, stringsIterator(moreLogs))
+			require.Error(t, err)
+			require.Equal(t, errOutOfSequence, err)
+
+			err = s.StoreLogs(baseIndex+1000, stringsIterator(moreLogs))
+			require.Error(t, err)
+			require.Equal(t, errOutOfSequence, err)
+		})
+	}
 }
 
 func TestEncoding(t *testing.T) {
