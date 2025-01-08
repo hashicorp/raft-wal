a28b6eb5368ef57d2d742da07e625d1f1b1c4aac
Matt Keeler <mjkeeler7@gmail.com>
2025-01-08 10:49:06 -0500

    Fix pkgsite docs formatting.

--- metrics/gometrics_collector.go
+++ metrics/gometrics_collector.go
@@ -4,33 +4,37 @@
 // # Metrics Configuration
 //
 // The raft-wal library is instrumented to be able to use different metrics collectors. There are currently two implemented within this package:
+//   - atomic
+//   - go-metrics
 //
-// * `atomic` -
-// * `go-metrics` -
+// # go-metrics Compatibility
 //
-// ## `go-metrics“ Compatibility
-// This library can emit metrics using either `github.com/armon/go-metrics` or `github.com/hashicorp/go-metrics`. Choosing between the libraries is controlled via build tags.
+// This library can emit metrics using either github.com/armon/go-metrics or github.com/hashicorp/go-metrics. Choosing between the libraries is controlled via build tags.
 //
-// **Build Tags**
-// * `armonmetrics` - Using this tag will cause metrics to be routed to `armon/go-metrics`
-// * `hashicorpmetrics` - Using this tag will cause all metrics to be routed to `hashicorp/go-metrics`
+// Build Tags:
+//   - armonmetrics - Using this tag will cause metrics to be routed to armon/go-metrics
+//   - hashicorpmetrics - Using this tag will cause all metrics to be routed to hashicorp/go-metrics
 //
-// If no build tag is specified, the default behavior is to use `armon/go-metrics`.
+// If no build tag is specified, the default behavior is to use armon/go-metrics.
 //
-// **Deprecating `armon/go-metrics`**
+// # Deprecating armon/go-metrics
 //
-// Emitting metrics to `armon/go-metrics` is officially deprecated. Usage of `armon/go-metrics` will remain the default until mid-2025 with opt-in support continuing to the end of 2025.
+// Emitting metrics to armon/go-metrics is officially deprecated. Usage of armon/go-metrics will remain the default until mid-2025 with opt-in support continuing to the end of 2025.
 //
-// **Migration**
-// To migrate an application currently using the older `armon/go-metrics` to instead use `hashicorp/go-metrics` the following should be done.
+// Migration:
+// To migrate an application currently using the older armon/go-metrics to instead use hashicorp/go-metrics the following should be done.
 //
-// 1. Upgrade libraries using `armon/go-metrics` to consume `hashicorp/go-metrics/compat` instead. This should involve only changing import statements. All repositories in the `hashicorp` namespace
-// 2. Update an applications library dependencies to those that have the compatibility layer configured.
-// 3. Update the application to use `hashicorp/go-metrics` for configuring metrics export instead of `armon/go-metrics`
-//   - Replace all application imports of `github.com/armon/go-metrics` with `github.com/hashicorp/go-metrics`
-//   - Instrument your build system to build with the `hashicorpmetrics` tag.
+//  1. Upgrade libraries using armon/go-metrics to consume hashicorp/go-metrics/compat instead. This should involve only changing import statements. All repositories within the hashicorp GitHub organization will be getting these updates in early 2025.
 //
-// Eventually once the default behavior changes to use `hashicorp/go-metrics` by default (mid-2025), you can drop the `hashicorpmetrics` build tag.
+//  2. Update an applications library dependencies to those that have the compatibility layer configured.
+//
+//  3. Update the application to use hashicorp/go-metrics for configuring metrics export instead of armon/go-metrics
+//
+//     - Replace all application imports of github.com/armon/go-metrics with github.com/hashicorp/go-metrics
+//
+//     - Instrument your build system to build with the hashicorpmetrics tag.
+//
+// Eventually once the default behavior changes to use hashicorp/go-metrics by default (mid-2025), you can drop the hashicorpmetrics build tag.
 package metrics
 
 import gometrics "github.com/hashicorp/go-metrics/compat"
